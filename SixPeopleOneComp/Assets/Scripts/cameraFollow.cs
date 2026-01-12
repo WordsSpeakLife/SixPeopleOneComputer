cf30ea67c1549349061579ca4abc1250afc53a7f
AKmarmion <aaronmarmion@gmail.com>
2026-01-12 12:52:47 -0500

    Made adjustment to camera and raycast walls
    Created script to have camera follow player but use LateUpdate() so it would move camera after update(). Dont want camera to move before player. Also added smoothSpeed to control how snappy or floaty the camera feels in scene. Used Lerp to get desired effect.
    
    Created wallFade script to use raycast from cameraPosition to player using vector3 to get the distance so it would only check what is between the camera and player not beyond. The reason for it is because I am using RaycastAll() and not Raycast. Putting all collider hit along the ray into a array. Great for multiple objects or walls blocking.

---
+++ SixPeopleOneComp/Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    [Header("Target")]
+    [SerializeField] Transform target;
+
+    [Header("Follow Settings")]
+    [SerializeField] Vector3 offset = new Vector3(0.0f, 14f, -14f);
+    [SerializeField] float smoothSpeed = 10f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void LateUpdate()
+    {
+        if (!target) return;
+
+        transform.position = Vector3.Lerp(transform.position, target.position, smoothSpeed * Time.deltaTime);
+
+    }
+}
---
+++ SixPeopleOneComp/Assets/Scripts/playerControl.cs
@@ -0,0 +1,75 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] int HP;
+    [SerializeField] int speed;
+    [SerializeField] int sprintMod;
+    [SerializeField] int jumpMax;
+    [SerializeField] int jumpSpeed;
+    [SerializeField] int gravity;
+
+    int jumpCount;
+    int HPOrig;
+
+    Vector3 moveDir;
+    Vector3 playerVelocity;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        HPOrig = HP; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+        sprint();
+    }
+
+    void movement()
+    {
+        if (controller.isGrounded)
+        {
+            jumpCount = 0;
+            playerVelocity = Vector3.zero;
+        }
+        else
+        {
+            playerVelocity.y -= gravity * Time.deltaTime;
+        }
+
+        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        controller.Move(moveDir * speed * Time.deltaTime);
+
+        jump();
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        
+    }
+
+    void jump()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            playerVelocity.y = jumpSpeed;
+            jumpCount++;
+        }
+    }
+
+    void sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+        }
+    }
+}
