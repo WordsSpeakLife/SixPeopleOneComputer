d85c2db1dcbe2d586984cb773b76c4bd9de659cd
Brian Pearson <brianusafpersonal@gmail.com>
2026-01-12 22:42:32 -0500

    Created Dev Scene added enemy scripts

---
+++ SixPeopleOneComp/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour, IDamage
+{
+
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Renderer model;
+    [SerializeField] Transform shootPos;
+
+    [SerializeField] int HP;
+    [SerializeField] int faceTargetSpeed;
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] float shootRate;
+
+
+    Color colorOrig;
+
+    float shootTimer;
+
+    Vector3 playerDir;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        colorOrig = model.material.color;
+        GameManager.instance.updateGameGoal(1);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shootTimer += Time.deltaTime;
+
+        playerDir = (GameManager.instance.player.transform.position - transform.position);
+
+        agent.SetDestination(GameManager.instance.player.transform.position);
+
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            faceTarget();
+        }
+
+        if(shootTimer >= shootRate)
+        {
+            shoot();
+        }
+    }
+
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x,transform.position.y,playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+
+        Instantiate(bullet, shootPos.position, transform.rotation);
+    }
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+
+        if (HP <= 0)
+        {
+            GameManager.instance.updateGameGoal(-1);
+            Destroy(gameObject);
+        }
+        else
+        {
+            StartCoroutine(flashRed());
+        }
+    }
+
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
+    }
+}
